da8d6149f18216d9d19efb793b6697cd45c2cd90
rishikkumar84a <rishikkumarchaurasiya@gmail.com>
2026-05-27 21:16:08 +0530

    feat(server): update server.js to include analytics routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./db/mongo');
 const authRoutes = require('./routes/auth');
 const leadsRoutes = require('./routes/leads');
 const activitiesRoutes = require('./routes/activities');
+const analyticsRoutes = require('./routes/analytics');
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/leads', leadsRoutes);
 app.use('/api', activitiesRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'BizFlow API is running!' });
